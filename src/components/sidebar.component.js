efa89cc08ee241be606933cf4b4d7d3839177fce
Gyxz <maks1404@gmail.com>
2020-05-06 15:07:30 +0300

    front home

--- src/components/sidebar.component.js
+++ src/components/sidebar.component.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
 export default class Sidebar extends Component {
   render() {
     return (
@@ -15,12 +14,26 @@ export default class Sidebar extends Component {
                     <Link to="/" className="nav-link">Послуги</Link>
                 </li>
                 <li className="navbar-item">
-                    <Link to="/" className="nav-link">Про нас</Link>
+                    <Link to="/" className="nav-link">Майстри</Link>
                 </li>
                 <li className="navbar-item">
                     <Link to="/" className="nav-link">Контакти</Link>
                 </li>
             </ul>
+        </div>
+        <div className="contact_block">
+            <div className="phone_block">
+                <p className=""> 
+                    <svg class="bi bi-phone" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M11 1H5a1 1 0 00-1 1v12a1 1 0 001 1h6a1 1 0 001-1V2a1 1 0 00-1-1zM5 0a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V2a2 2 0 00-2-2H5z" clip-rule="evenodd"/>
+                    <path fill-rule="evenodd" d="M8 14a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/>
+                    </svg>
+                    +380963526992
+                </p>
+            </div>
+            <div className="email_block">
+                <p className="">sweethome@gmail.com</p>
+            </div>
         </div>
     </nav>
     
