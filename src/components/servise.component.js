85c6be25822fef7a1ea065a80c580f57d994b0de
Gyxz <maks1404@gmail.com>
2020-05-10 12:03:05 +0300

    добавив вивід з бд

--- src/components/servise.component.js
+++ src/components/servise.component.js
@@ -3,15 +3,33 @@ import axios from 'axios'
 import {  Col, Row } from 'antd';
 import Card from 'react-bootstrap/Card'
 
+function UserCardBlock(props) {
+
+        const renderItems = () => (
+            props.products && props.products.map(item => (
+                <div key={item._id}>
+                    <p>Type_service_name: {item.type_service_name}</p>
+                    <p>Price: {item.price}</p>
+                </div>
+            ))
+        )
+        return (
+            <div>
+                
+                {renderItems()}
+                    
+            </div>
+        )
+    }
 const Service = props => (
 
     <Col lg={6} md={8} xs={24}>
         <Card border="dark" style={{ width: '18rem' }}>
             <Card.Header>{props.service.servicename}</Card.Header>
             <Card.Body>
-                <Card.Title> {props.service.type_service[3].type_service_name} </Card.Title>
+                <Card.Title> {props.service.type_service.price} </Card.Title>
                 <Card.Text>
-                    
+                    <UserCardBlock products={props.service.type_service}/>
                 </Card.Text>
             </Card.Body>
         </Card>
@@ -19,19 +37,29 @@ const Service = props => (
     </Col>
     
   )
+  
 
 export default class ServiceList extends Component {
     constructor(props){
         super(props);
 
-        this.state = {services: [],type_service: []};
+        this.state = {services: []};
     }
+
+    renderItems() {
+        return this.state.type_service.map(item => (
+          <div key={item._id}>
+            <p>Type_service_name: {item.type_service_name}</p>
+            <p>Price: {item.price}</p>
+          </div>
+        ));
+      }
 
     componentDidMount(){
         axios.get('http://localhost:5000/service/')
             .then(res =>{
                 this.setState({services: res.data})
-                this.setState({type_service: res.data})
+                
             })
             .catch((error) => {
                 console.log(error);
